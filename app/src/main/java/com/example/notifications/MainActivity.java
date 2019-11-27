d200ae1c59d06c3b7daa2fcc46b365914c326860
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-11-27 22:14:34 +0530

    Notification channels

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.notifications;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationManagerCompat notificationManagerCompat;
+    private EditText editTextTitle;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextMessage = findViewById(R.id.edit_text_message);
+    }
+
+    public void sendOnChannel1(View v) {
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_looks_one_black_24dp)
+                .setContentTitle(editTextTitle.getText().toString().trim())
+                .setContentText(editTextMessage.getText().toString().trim())
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(1, notification);
+    }
+
+    public void sendOnChannel2(View v) {
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_looks_two_black_24dp)
+                .setContentTitle(editTextTitle.getText().toString().trim())
+                .setContentText(editTextMessage.getText().toString().trim())
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .build();
+
+        notificationManagerCompat.notify(2, notification);
     }
 }
